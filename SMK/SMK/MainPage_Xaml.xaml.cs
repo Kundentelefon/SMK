5e581661bad28ca1edb5ea155aebac3207531144
Martin Eckstein <ow.gaming@gmx.de>
2015-09-08 15:26:43 +0200

    Penis

---
+++ SMK/SMK/MainPage_Xaml.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SMK
+{
+    public partial class MainPage_Xaml : ContentPage
+    {
+        public MainPage_Xaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
