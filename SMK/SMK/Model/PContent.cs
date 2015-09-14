0006c1039676590d4aaf63b32ee06dcf5e99887c
Sebastian Schlund <sebastian.o.schlund@gmail.com>
2015-09-15 01:23:02 +0200

    Added Dummies und Update modelsBeispiel Bilder+html

--- SMK/SMK/Model/PContent.cs
+++ SMK/SMK/Model/PContent.cs
@@ -11,6 +11,15 @@ namespace SMK.Model
         public int content_ID { get; set; }
         public int content_Kind { get; set; }
         public String content_Title { get; set; }
-        public String content_Location { get; set; }
+        //Namen der Elemente im Orderner // FischerTechnik/PContent/0(ContentID)
+        public List<String> content_FileNames { get; set; }
+
+        public PContent(int content_ID, int content_Kind, String content_Title, List<String> content_FileNames)
+        {
+            this.content_ID = content_ID;
+            this.content_Kind = content_Kind;
+            this.content_FileNames = content_FileNames;
+            this.content_Title = content_Title;
+        }
     }
 }
---
+++ SMK/SMK/Support/localFileSystem.cs
@@ -0,0 +1,46 @@
+﻿using SMK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMK.Support
+{
+    class localFileSystem
+    {
+        public List<Product> loadProductList()
+        {
+            List<Product> returnList = new List<Product> { new Product(0, "Test","Resources/FischerTechnik/Files/0.png", "havesomeTextShit i hope you like it", new List<int>(new int[] { 0, 1, 2 })) ,
+                                                           new Product(1, "TestProductNotOwned",  "Resources/FischerTechnik/Files/1.png", "havesomeTextShit i hope you like it", new List<int>(new int[] { 3, 4})),
+                                                           new Product(2, "TestProductNotDownloaded",  "Resources/FischerTechnik/Files/2.png", "havesomeTextShit i hope you like it", new List<int>(new int[] { 5, 6 }))
+                                                            };
+
+            //dummies
+            //Product testpro = new Product();
+            //List<string> optionList = new List<string> { "string1", "string2", ..., "stringN"}; 
+            //List<int> testintl = new List<int>(new int[] { 1, 2, 3 });
+            //Product testpro = new Product(0, "Test", "NochKeinPlan",  "havesomeText", new List<int>(new int[] { 1, 2, 3 }));
+            //210.210
+            
+            return (returnList);
+        }
+        public List<PContent> loadContentList() { 
+
+            List<PContent> returnList = new List<PContent> {new PContent(0,0,"BilderGalarie", new List<String>(new String[] {"da.png","haha.png","devil.png"})),//"Resources/FischerTechnik/PContent/0_Icon.png"
+                                                           new PContent(1,0,"BilderGalarie",new List<String>(new String[]{"da.png","haha.png","devil.png"})),
+                                                           new PContent(2,2,"WebView",new List<String>(new String[]{})),
+                                                           new PContent(5,0,"BilderGalarie",new List<String>(new String[]{"da.png","haha.png","devil.png"})),//für diese Fehlt noch der Content da dieser Noch nicht "gedownloaded" wurde
+                                                           new PContent(6,0,"BilderGalarie",new List<String>(new String[]{"da.png","haha.png","devil.png"}))//für diese Fehlt noch der Content da dieser Noch nicht "gedownloaded" wurde
+                                                            };
+            return (returnList);
+        }
+        public Boolean saveModelsLocal(List<Product> inputProducts, List<PContent> inputContents)
+        {
+            Boolean returnSuccess = false;
+
+
+            return (returnSuccess);
+        }
+    }
+}
