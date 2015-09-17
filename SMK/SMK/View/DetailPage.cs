b0cc39476203742e4ea3baa6f3811f92b7c96f1d
Martin Eckstein <ow.gaming@gmx.de>
2015-09-17 18:36:16 +0200

    Detailseite: Imagestack hinzugefügt, Carouselpage hinzugefügt

--- SMK/SMK/View/DetailPage.cs
+++ SMK/SMK/View/DetailPage.cs
@@ -17,52 +17,30 @@ namespace SMK.View
         Product product;
         localFileSystem files;
         List<PContent> pContent;
+        bool owned;
+        List<PContent> contentPictureGalarie;
+        List<PContent> contentPdf;
+        List<PContent> contentHtml;
+        List<PContent> contentVideo;
+
         public DetailPage(Product ResourceProduct)
         {
             product = ResourceProduct;
             files = new localFileSystem();
             pContent = files.loadContentList(product);
             StackLayout stackLayout = new StackLayout();
-            StackLayout buttonStack = new StackLayout();
+            StackLayout imageStack = new StackLayout();
+            ScrollView imageScroll = new ScrollView();
             TapGestureRecognizer gesture = new TapGestureRecognizer();
-            List<PContent> contentPictureGalarie = new List<PContent>() ;
-            List<PContent> contentPdf = new List<PContent>();
-            List<PContent> contentHtml = new List<PContent>();
-            List<PContent> contentVideo = new List<PContent>();
-            bool owned = false;
+            contentPictureGalarie = new List<PContent>() ;
+            contentPdf = new List<PContent>();
+            contentHtml = new List<PContent>();
+            contentVideo = new List<PContent>();
+            owned = false;
             Color color = Color.FromHex("E2001A");
 
-
-            if (pContent[0] != null)
-            {
-                owned = true;
-                foreach (PContent content in pContent)
-                {
-                    if (content.content_Kind == 0)
-                    {
-                        contentPictureGalarie.Add(content);
-                        //SMK.FischerTechnik.Files.1.png
-                        //picture_galarie.Add(imagePfadContent+content.content_ID.ToString()+".png");
-                        
-                    }
-                    if (content.content_Kind == 1)
-                    {
-                        contentPdf.Add(content);
-
-                    }
-                    if (content.content_Kind == 2)
-                    {
-                        contentHtml.Add(content);
-
-                    }
-                    if (content.content_Kind == 3)
-                    {
-                        contentVideo.Add(content);
-
-                    }
-
-                }
-            }
+            initContentLists();
+           
             String image_path = imagePfadProduct + product.product_ID.ToString() + ".png";
             stackLayout.Children.Add(
             new Image
@@ -89,12 +67,81 @@ namespace SMK.View
                     }
                 }
                 );//Child Added: Klappentext
-           
+
+            //Images des Produktes werden initalisiert
+            imageScroll.Orientation = ScrollOrientation.Horizontal;
+            imageStack.Orientation = StackOrientation.Horizontal;
+            List<ContentPage> pages = new List<ContentPage>();
+            TapGestureRecognizer reco = new TapGestureRecognizer();
+            CarouselPage carousel = new CarouselPage();
+
+            foreach (PContent content in contentPictureGalarie)
+            {
+                foreach (string image_source in content.content_FileNames)
+                {
+                    string source = imagePfadContent + image_source;
+                  
+                    Frame frame = new Frame
+                    {
+                        OutlineColor = color,
+                        Content = new Image
+                        {
+                            Source = ImageSource.FromResource(source)
+                        }
+                    };
+                    imageStack.Children.Add(frame);
+                    carousel.Children.Add(new ContentPage { Content = new Image { Source = ImageSource.FromResource(source) } });
+                    
+                }
+            }
+
             
+            reco.Tapped += async (sender, e) =>
+            {
+                await Navigation.PushAsync(carousel);
+            };
+            imageStack.GestureRecognizers.Add(reco);
+            imageScroll.Content = imageStack;
+            stackLayout.Children.Add(imageScroll);
+            //Image scrolllayout hinzugefügt
 
             Content = stackLayout;
             BackgroundColor = Color.White;
             Padding = new Thickness(5, Device.OnPlatform(0, 15, 0), 5, 5);
         }//ende Construktor
+
+        public void initContentLists()
+        {
+            if (pContent[0] != null)
+            {
+                owned = true;
+                foreach (PContent content in pContent)
+                {
+                    if (content.content_Kind == 0)
+                    {
+                        contentPictureGalarie.Add(content);
+                        //SMK.FischerTechnik.Files.1.png
+                        //picture_galarie.Add(imagePfadContent+content.content_ID.ToString()+".png");
+
+                    }
+                    if (content.content_Kind == 1)
+                    {
+                        contentPdf.Add(content);
+
+                    }
+                    if (content.content_Kind == 2)
+                    {
+                        contentHtml.Add(content);
+
+                    }
+                    if (content.content_Kind == 3)
+                    {
+                        contentVideo.Add(content);
+
+                    }
+
+                }
+            }
+        }
     }
 }
